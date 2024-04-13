8a14c12850f7a26683a655e72b5f21f80c3725ec
Sugyan98 <sugyanmahakud@gmail.com>
2024-04-13 23:39:04 +0530

    Adding Summary

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,13 @@
+import { Fragment } from "react";
+
+import MealsSummary from "./MealsSummary";
+
+const Meals = () => {
+  return (
+    <Fragment>
+      <MealsSummary />
+    </Fragment>
+  );
+};
+
+export default Meals;
